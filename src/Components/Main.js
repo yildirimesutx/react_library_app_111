d7e5cf9293ba8e76668ab2507ffe87ec57c1f24c
mesut <yildirimesutcw@gmail.com>
2022-05-31 22:44:07 +0300

    library app

---
+++ src/Components/Main.js
@@ -0,0 +1,42 @@
+import React, { useState }  from 'react';
+import Card from "./Card";
+import axios from "axios";
+
+const Main=()=>{
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+    return(
+        <>
+            <div className="header">
+                <div className="row1">
+                    <h1>“Learn from yesterday, live for today, hope for tomorrow.”</h1>
+                </div>
+                <div className="row2">
+                    <h2>find a book...</h2>
+                    <div className="search">
+                        <input type="text" placeholder="enter book name..."
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress={searchBook}/>
+                        <button><i className="fas fa-search"></i></button>
+                    </div>
+                    <img src="./images/bg2.png" alt="" />
+                </div>
+            </div>
+
+            <div className="container">
+              {
+                    <Card book={bookData}/>
+              }  
+            </div>
+        </>
+    )
+}
+export default Main;
